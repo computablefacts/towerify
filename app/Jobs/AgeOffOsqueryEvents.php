1f730f50b42a4ccf3d3686defaa4341378b05041
csavelief <csavelief@mncc.fr>
2024-05-21 18:51:22 +0200

    Closes #24

--- app/Jobs/AgeOffOsqueryEvents.php
+++ app/Jobs/AgeOffOsqueryEvents.php
@@ -32,24 +32,5 @@ class AgeOffOsqueryEvents implements ShouldQueue
             WHERE calendar_time <= '{$min->toDateString()}'
             AND name IN ('memory_available_snapshot', 'disk_available_snapshot')
         ");
-
-        // Deduplicate events
-        $min = Carbon::today()->subHours(1);
-        DB::delete("
-            DELETE t1 FROM ynh_osquery t1
-            INNER JOIN ynh_osquery t2
-            WHERE t1.id < t2.id
-            AND t1.calendar_time >= '{$min->toDateString()}'
-            AND t1.ynh_server_id = t2.ynh_server_id
-            AND t1.name = t2.name
-            AND t1.host_identifier = t2.host_identifier
-            AND t1.calendar_time = t2.calendar_time
-            AND t1.unix_time = t2.unix_time
-            AND t1.epoch = t2.epoch
-            AND t1.counter = t2.counter
-            AND t1.numerics = t2.numerics
-            AND t1.columns = t2.columns
-            AND t1.action = t2.action;
-        ");
     }
 }
