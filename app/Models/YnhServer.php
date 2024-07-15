56fb418ae698a4df190cb2cd7095a9f8f888114a
Patrick Brisacier <pbrisacier@mncc.fr>
2024-07-15 13:58:06 +0200

    YunoHost username should be in lowercase

--- app/Models/YnhServer.php
+++ app/Models/YnhServer.php
@@ -1068,7 +1068,6 @@ EOT;
         }
 
         $fullname = preg_replace("/[^A-Za-z0-9 ,.'-]/", '', $fullname);
-        $fullname = Str::lower($fullname);
         $password = Str::replace('!', '\!', $password); // history substitution
         $ssh->newTrace(SshTraceStateEnum::IN_PROGRESS, 'Creating user profile...');
         $isOk = $ssh->executeCommand("sudo yunohost user create {$username} -F \"{$fullname}\" -p \"{$password}\" -d {$domain->name}", $output);
