ee64af50c3a077347a19c697b28beab7bc55d8e0
csavelief <csavelief@mncc.fr>
2024-09-12 15:31:08 +0200

    Honeypots - Fix the impact endpoint

--- app/Modules/AdversaryMeter/Http/Controllers/HoneypotController.php
+++ app/Modules/AdversaryMeter/Http/Controllers/HoneypotController.php
@@ -104,6 +104,7 @@ class HoneypotController extends Controller
             ->join('scans', 'scans.id', '=', 'ports.scan_id')
             ->join('assets', 'assets.cur_scan_id', '=', 'scans.ports_scan_id')
             ->get()
+            ->filter(fn(Alert $alert) => !$attackerId || $alert->events($attackerId)->exists())
             ->map(function (Alert $alert) use ($attackerId) {
                 return [
                     'alert' => $alert,
