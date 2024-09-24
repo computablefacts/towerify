ae69af9089d12e2f99826bfc8aed5d677bcbb987
csavelief <csavelief@mncc.fr>
2024-09-24 16:00:13 +0200

    Code refactoring

--- app/Modules/AdversaryMeter/Helpers/ApiUtils.php
+++ app/Modules/AdversaryMeter/Helpers/ApiUtils.php
@@ -8,12 +8,13 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\RequestOptions;
 use Illuminate\Support\Facades\Config;
+use Psr\Http\Message\ResponseInterface;
 
 class ApiUtils
 {
     const REQUEST_TIMEOUT = 300; // 5 minutes
 
-    public function checkport_public($host, $port, $protocol)
+    public function checkport_public($host, $port, $protocol): array
     {
         $response = $this->post('/checkport', [
             'host' => $host,
@@ -23,7 +24,169 @@ class ApiUtils
         return $this->json($response);
     }
 
-    private function post($endpoint, $json)
+    public function discover_public(string $domain): array
+    {
+        $response = $this->post('/discover', array_merge(
+            [
+                'domain' => $domain,
+            ]
+        ));
+        return $this->json($response);
+    }
+
+    public function screenshot_public(string $domain): array
+    {
+        $response = $this->post('/screenshot', array_merge(
+            [
+                'url' => $domain,
+            ]
+        ));
+        return $this->json($response);
+    }
+
+    public function start_scan_public($asset, $ip, $port, $protocol): array
+    {
+        $response = $this->post('/start_scan', array_merge(
+            [
+                'hostname' => $asset,
+                'ip' => $ip,
+                'port' => $port,
+                'protocol' => $protocol,
+                'client' => null,
+                'tags' => [],
+                'tests' => [],
+            ]
+        ));
+        return $this->json($response);
+    }
+
+    public function task_masscan_public(string $host): array
+    {
+        $response = $this->post('/task-masscan', [
+            'input' => [
+                $host
+            ]
+        ]);
+        return $this->json($response);
+    }
+
+    public function task_nmap_public(string $host): array
+    {
+        $response = $this->post('/task-nmap', [
+            'input' => [
+                $host
+            ]
+        ]);
+        return $this->json($response);
+    }
+
+    public function task_status_public(string $id): array
+    {
+        $response = $this->get('/task_status/' . $id, []);
+        return $this->json($response);
+    }
+
+    public function task_result_public(string $id): array
+    {
+        $response = $this->get('/task_result/' . $id, []);
+        return $this->json($response);
+    }
+
+    public function task_start_scan_public($hostname, $ip, $port, $protocol, $tags): array
+    {
+        $response = $this->post('/start_scan', [
+            "hostname" => $hostname,
+            "ip" => $ip,
+            "port" => $port,
+            "protocol" => $protocol,
+            "client" => "",
+            "tags" => $tags
+        ]);
+        return $this->json($response);
+    }
+
+    public function task_get_scan_public(string $scanId): array
+    {
+        $response = $this->get('/get_scan/' . $scanId, []);
+        return $this->json($response);
+    }
+
+    public function ip_geoloc_public(string $ip): array
+    {
+        $response = $this->post('/ipgeoloc', [
+            'input' => $ip
+        ]);
+        return $this->json($response);
+    }
+
+    public function ip_whois_public(string $ip): array
+    {
+        $response = $this->post('/ipwhois', [
+            'input' => $ip
+        ]);
+        return $this->json($response);
+    }
+
+    public function task_discover_full_public(array $urls): array
+    {
+        $response = $this->post('/discover-full-task', [
+            'input' => $urls
+        ]);
+        return $this->json($response);
+    }
+
+    public function discover_from_ip_public(string $ip): array
+    {
+        $response = $this->post('/rapid_reverse_ip', [
+            'input' => $ip
+        ]);
+        return $this->json($response);
+    }
+
+    public function external_ips(): array
+    {
+        $response = $this->external_ips_private();
+        return $this->json($response);
+    }
+
+    public function matched_cves(): array
+    {
+        $response = $this->matched_cves_private();
+        return $this->json($response);
+    }
+
+    private function external_ips_private(): ResponseInterface
+    {
+        // ../.. to remove /api/v1/ so go back to root.
+        return $this->get('/../../sentinel_external_ips', []);
+    }
+
+    private function matched_cves_private(): ResponseInterface
+    {
+        // ../.. to remove /api/v1/ so go back to root.
+        return $this->get('/../../cves-list', []);
+    }
+
+    private function get($endpoint, $json): ResponseInterface
+    {
+        $url = Config::get('towerify.adversarymeter.api') . $endpoint;
+        try {
+            $client = new Client([
+                RequestOptions::TIMEOUT => self::REQUEST_TIMEOUT,
+                'auth' => [
+                    config('towerify.adversarymeter.api_username'),
+                    config('towerify.adversarymeter.api_password')
+                ]
+            ]);
+            return $client->get($url, $this->httpHeaders($json));
+        } catch (ClientException $exception) {
+            throw new ApiException('API client problem (code: ' . $exception->getCode() . ', url: ' . $url . ')', $exception->getCode(), $exception);
+        } catch (Exception $exception) {
+            throw new ApiException('API problem: ' . $exception->getMessage(), $exception->getCode(), $exception);
+        }
+    }
+
+    private function post($endpoint, $json): ResponseInterface
     {
         $url = Config::get('towerify.adversarymeter.api') . $endpoint;
         try {
@@ -42,7 +205,7 @@ class ApiUtils
         }
     }
 
-    private function httpHeaders($json)
+    private function httpHeaders($json): array
     {
         return [
 
@@ -61,13 +224,13 @@ class ApiUtils
         ];
     }
 
-    private function json($response, $dataset = null)
+    private function json(ResponseInterface $response): array
     {
         $body = $this->body($response);
-        return $this->isOk($response, $body) ? $this->results($response, $body, $dataset) : [];
+        return $this->isOk($response, $body) ? $this->results($response, $body) : [];
     }
 
-    public function body($response)
+    private function body(ResponseInterface $response): array
     {
         ini_set("memory_limit", "-1");
         if (version_compare(PHP_VERSION, '5.4.0', '>=')
@@ -91,178 +254,16 @@ class ApiUtils
         return json_decode($json_without_bigints, true);
     }
 
-    public function isOk($response, $body)
+    private function isOk(ResponseInterface $response, array $body): bool
     {
         if ($response->getStatusCode() === 200) {
             return true;
         }
-        throw new ApiException('Fast API problem: ' . (isset($body['message']) ? $body['message'] : 'unknown'));
+        throw new ApiException('Fast API problem: ' . ($body['message'] ?? 'unknown'));
     }
 
-    private function results($response, $body, $dataset = null)
+    private function results(ResponseInterface $response, array $body): array
     {
         return $this->isOk($response, $body) && !empty($body) ? $body : [];
-    }
-
-    public function discover_public(string $domain)
-    {
-        $response = $this->post('/discover', array_merge(
-            [
-                'domain' => $domain,
-            ]
-        ));
-        return $this->json($response, $domain);
-    }
-
-    public function screenshot_public(string $domain)
-    {
-        $response = $this->post('/screenshot', array_merge(
-            [
-                'url' => $domain,
-            ]
-        ));
-        return $this->json($response, $domain);
-    }
-
-    public function start_scan_public($asset, $ip, $port, $protocol)
-    {
-        $response = $this->post('/start_scan', array_merge(
-            [
-                'hostname' => $asset,
-                'ip' => $ip,
-                'port' => $port,
-                'protocol' => $protocol,
-                'client' => null,
-                'tags' => [],
-                'tests' => [],
-            ]
-        ));
-        return $this->json($response, $asset);
-    }
-
-    public function task_masscan_public(string $host)
-    {
-        $response = $this->post('/task-masscan', [
-            'input' => [
-                $host
-            ]
-        ]);
-        return $this->json($response, $host);
-    }
-
-    public function task_nmap_public(string $host)
-    {
-        $response = $this->post('/task-nmap', [
-            'input' => [
-                $host
-            ]
-        ]);
-        return $this->json($response, $host);
-    }
-
-    public function task_status_public(string $id)
-    {
-        $response = $this->get('/task_status/' . $id, []);
-        return $this->json($response, $id);
-    }
-
-    private function get($endpoint, $json)
-    {
-        $url = Config::get('towerify.adversarymeter.api') . $endpoint;
-        try {
-            $client = new Client([
-                RequestOptions::TIMEOUT => self::REQUEST_TIMEOUT,
-                'auth' => [
-                    config('towerify.adversarymeter.api_username'),
-                    config('towerify.adversarymeter.api_password')
-                ]
-            ]);
-            return $client->get($url, $this->httpHeaders($json));
-        } catch (ClientException $exception) {
-            throw new ApiException('API client problem (code: ' . $exception->getCode() . ', url: ' . $url . ')', $exception->getCode(), $exception);
-        } catch (Exception $exception) {
-            throw new ApiException('API problem: ' . $exception->getMessage(), $exception->getCode(), $exception);
-        }
-    }
-
-    public function task_result_public(string $id)
-    {
-        $response = $this->get('/task_result/' . $id, []);
-        return $this->json($response, $id);
-    }
-
-    public function task_start_scan_public($hostname, $ip, $port, $protocol, $tags)
-    {
-        $response = $this->post('/start_scan', [
-            "hostname" => $hostname,
-            "ip" => $ip,
-            "port" => $port,
-            "protocol" => $protocol,
-            "client" => "",
-            "tags" => $tags
-        ]);
-        return $this->json($response, $hostname);
-    }
-
-    public function task_get_scan_public(string $scanId)
-    {
-        $response = $this->get('/get_scan/' . $scanId, []);
-        return $this->json($response, $scanId);
-    }
-
-    public function ip_geoloc_public(string $ip)
-    {
-        $response = $this->post('/ipgeoloc', [
-            'input' => $ip
-        ]);
-        return $this->json($response, $ip);
-    }
-
-    public function ip_whois_public(string $ip)
-    {
-        $response = $this->post('/ipwhois', [
-            'input' => $ip
-        ]);
-        return $this->json($response, $ip);
-    }
-
-    public function task_discover_full_public(array $urls)
-    {
-        $response = $this->post('/discover-full-task', [
-            'input' => $urls
-        ]);
-        return $this->json($response);
-    }
-
-    public function discover_from_ip_public(string $ip)
-    {
-        $response = $this->post('/rapid_reverse_ip', [
-            'input' => $ip
-        ]);
-        return $this->json($response, $ip);
-    }
-
-    public function external_ips()
-    {
-        $response = $this->external_ips_private();
-        return $this->json($response);
-    }
-
-    public function matched_cves()
-    {
-        $response = $this->matched_cves_private();
-        return $this->json($response);
-    }
-
-    private function external_ips_private()
-    {
-        // ../.. to remove /api/v1/ so go back to root.
-        return $this->get('/../../sentinel_external_ips', []);
-    }
-
-    private function matched_cves_private()
-    {
-        // ../.. to remove /api/v1/ so go back to root.
-        return $this->get('/../../cves-list', []);
     }
 }
