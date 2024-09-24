df32666d3b651fc5cc638f6d95332c32dd17e846
csavelief <csavelief@mncc.fr>
2024-09-24 14:24:12 +0200

    Cleanup

--- config/app.php
+++ config/app.php
@@ -226,8 +226,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'ApiUtils' => \App\Modules\AdversaryMeter\Helpers\ApiUtilsFacade::class,
-        'ApiUtilsCb' => \App\Modules\CyberBuddy\Helpers\ApiUtilsFacade::class,
 
     ],
 
