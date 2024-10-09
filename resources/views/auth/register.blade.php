73cded5350d2a9a8fbfde0df469b199c1a80d138
csavelief <csavelief@mncc.fr>
2024-10-09 18:36:03 +0200

    Fixup! Auth - Only allow uppercase/lowercase letters and digits in the register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -57,7 +57,7 @@
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                <input id="password-confirm" placeholder="{{ __('uppercase/lowercase letters and digits only') }}" type="password" class="form-control" name="password_confirmation" required>
                             </div>
                         </div>
 
