298e9867eb2f24130ac4284b1684f56bb62c8502
csavelief <csavelief@mncc.fr>
2024-10-17 15:51:28 +0200

    Translate _payment.blade.php

--- resources/views/checkout/_payment.blade.php
+++ resources/views/checkout/_payment.blade.php
@@ -1,30 +1,26 @@
 <div id="payment" class="mb-4">
-    <h3>Payment Method</h3>
-    <hr>
-
-    <div class="mb-3 row">
-
-        <div class="col-md-11 offset-md-1">
-        @forelse($paymentMethods as $paymentMethod)
-            <div class="form-check">
-                {{ Form::radio('paymentMethod', $paymentMethod->id, count($paymentMethods) === 1, [
-                        'class' => 'form-check-input' . ($errors->has('paymentMethod') ? ' is-invalid' : ''),
-                        'id' => 'payment_method_' . $paymentMethod->id
-                    ])
-                }}
-                <label class="form-check-label" for="{{ 'payment_method_' . $paymentMethod->id }}">
-                    {{ $paymentMethod->name }}
-                </label>
-            </div>
-        @empty
-        @endforelse
-        </div>
-
-        @if ($errors->has('paymentMethod'))
-            <div class="col-12 mt-2">
-                <div class="alert alert-danger">{{ $errors->first('paymentMethod') }}</div>
-            </div>
-        @endif
+  <h3>{{ __('Payment Method') }}</h3>
+  <hr>
+  <div class="mb-3 row">
+    <div class="col-md-11 offset-md-1">
+      @forelse($paymentMethods as $paymentMethod)
+      <div class="form-check">
+        {{ Form::radio('paymentMethod', $paymentMethod->id, count($paymentMethods) === 1, [
+        'class' => 'form-check-input' . ($errors->has('paymentMethod') ? ' is-invalid' : ''),
+        'id' => 'payment_method_' . $paymentMethod->id
+        ]) }}
+        <label class="form-check-label" for="{{ 'payment_method_' . $paymentMethod->id }}">
+          {{ __($paymentMethod->name) }}
+        </label>
+      </div>
+      @empty
+      @endforelse
     </div>
+    @if ($errors->has('paymentMethod'))
+    <div class="col-12 mt-2">
+      <div class="alert alert-danger">{{ $errors->first('paymentMethod') }}</div>
+    </div>
+    @endif
+  </div>
 </div>
 
