0dd259e11f2471f206dbeac03bc318c1b98c5e41
csavelief <csavelief@mncc.fr>
2024-11-07 18:35:04 +0100

    Auth/Invitation - Password was wrongly set using bcrypt instead of Laravel's Hasher class

--- app/Models/Invitation.php
+++ app/Models/Invitation.php
@@ -2,20 +2,47 @@
 
 namespace App\Models;
 
+use App\Hashing\TwHasher;
 use App\Traits\HasTenant;
 use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Model;
 use Konekt\AppShell\Models\Invitation as InvitationBase;
+use Konekt\User\Contracts\User as UserContract;
+use Konekt\User\Events\UserInvitationUtilized;
+use Konekt\User\Events\UserIsBeingCreatedFromInvitation;
+use Konekt\User\Models\UserProxy;
 
 class Invitation extends InvitationBase
 {
     use HasTenant;
 
-    public function scopePending(Builder $query)
+    public function createUser(array $furtherAttributes = [], string $userClass = null, bool $dontEncryptPassword = false): UserContract
     {
-        return $query
-            ->whereNull(DB::raw('invitations.user_id'))
-            ->where('expires_at', '>', Carbon::now()->toDateTimeString());
+        $attributes = array_merge([
+            'email' => $this->email,
+            'name' => $this->name,
+            'type' => $this->type,
+        ], $furtherAttributes);
+
+        if (!$dontEncryptPassword && isset($attributes['password'])) {
+            $attributes['password'] = TwHasher::hash($attributes['password']);
+        }
+
+        $userClass = $userClass ?? UserProxy::modelClass();
+        /** @var Model $user */
+        $user = new $userClass();
+        $user->fill($attributes);
+
+        event(new UserIsBeingCreatedFromInvitation($this, $user));
+
+        $user->push();
+
+        $this->user_id = $user->id;
+        $this->utilized_at = Carbon::now();
+        $this->save();
+
+        event(new UserInvitationUtilized($this));
+
+        return $user;
     }
 }
