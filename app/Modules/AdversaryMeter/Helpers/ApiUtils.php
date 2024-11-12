4785e2809f92e965c53d5ed2fe059f2cab8de4a5
csavelief <csavelief@mncc.fr>
2024-11-12 18:24:45 +0100

    AdversaryMeter - Remove dependency to 'Guzzle' : use Laravel's 'Http' class instead

--- app/Modules/AdversaryMeter/Helpers/ApiUtils.php
+++ app/Modules/AdversaryMeter/Helpers/ApiUtils.php
@@ -2,36 +2,28 @@
 
 namespace App\Modules\AdversaryMeter\Helpers;
 
-use App\Modules\AdversaryMeter\Exceptions\ApiException;
-use Exception;
-use GuzzleHttp\Client;
-use GuzzleHttp\Exception\ClientException;
-use GuzzleHttp\RequestOptions;
 use Illuminate\Support\Facades\Config;
-use Psr\Http\Message\ResponseInterface;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class ApiUtils
 {
-    const REQUEST_TIMEOUT = 300; // 5 minutes
-
     public function checkport_public($host, $port, $protocol): array
     {
-        $response = $this->post('/checkport', [
+        return $this->post('/checkport', [
             'host' => $host,
             'port' => $port,
             'protocol' => $protocol
         ]);
-        return $this->json($response);
     }
 
     public function discover_public(string $domain): array
     {
-        $response = $this->post('/discover', array_merge(
+        $json = $this->post('/discover', array_merge(
             [
                 'domain' => $domain,
             ]
         ));
-        $json = $this->json($response);
         if (isset($json['subdomains'])) {
             $json['subdomains'] = collect($json['subdomains'])
                 ->filter(fn(string $subdomain) => !empty($subdomain))
@@ -42,17 +34,16 @@ class ApiUtils
 
     public function screenshot_public(string $domain): array
     {
-        $response = $this->post('/screenshot', array_merge(
+        return $this->post('/screenshot', array_merge(
             [
                 'url' => $domain,
             ]
         ));
-        return $this->json($response);
     }
 
     public function start_scan_public($asset, $ip, $port, $protocol): array
     {
-        $response = $this->post('/start_scan', array_merge(
+        return $this->post('/start_scan', array_merge(
             [
                 'hostname' => $asset,
                 'ip' => $ip,
@@ -63,44 +54,39 @@ class ApiUtils
                 'tests' => [],
             ]
         ));
-        return $this->json($response);
     }
 
     public function task_masscan_public(string $host): array
     {
-        $response = $this->post('/task-masscan', [
+        return $this->post('/task-masscan', [
             'input' => [
                 $host
             ]
         ]);
-        return $this->json($response);
     }
 
     public function task_nmap_public(string $host): array
     {
-        $response = $this->post('/task-nmap', [
+        return $this->post('/task-nmap', [
             'input' => [
                 $host
             ]
         ]);
-        return $this->json($response);
     }
 
     public function task_status_public(string $id): array
     {
-        $response = $this->get('/task_status/' . $id, []);
-        return $this->json($response);
+        return $this->get('/task_status/' . $id);
     }
 
     public function task_result_public(string $id): array
     {
-        $response = $this->get('/task_result/' . $id, []);
-        return $this->json($response);
+        return $this->get('/task_result/' . $id);
     }
 
     public function task_start_scan_public($hostname, $ip, $port, $protocol, $tags): array
     {
-        $response = $this->post('/start_scan', [
+        return $this->post('/start_scan', [
             "hostname" => $hostname,
             "ip" => $ip,
             "port" => $port,
@@ -108,45 +94,39 @@ class ApiUtils
             "client" => "",
             "tags" => $tags
         ]);
-        return $this->json($response);
     }
 
     public function task_get_scan_public(string $scanId): array
     {
-        $response = $this->get('/get_scan/' . $scanId, []);
-        return $this->json($response);
+        return $this->get('/get_scan/' . $scanId);
     }
 
     public function ip_geoloc_public(string $ip): array
     {
-        $response = $this->post('/ipgeoloc', [
+        return $this->post('/ipgeoloc', [
             'input' => $ip
         ]);
-        return $this->json($response);
     }
 
     public function ip_whois_public(string $ip): array
     {
-        $response = $this->post('/ipwhois', [
+        return $this->post('/ipwhois', [
             'input' => $ip
         ]);
-        return $this->json($response);
     }
 
     public function task_discover_full_public(array $urls): array
     {
-        $response = $this->post('/discover-full-task', [
+        return $this->post('/discover-full-task', [
             'input' => $urls
         ]);
-        return $this->json($response);
     }
 
     public function discover_from_ip_public(string $ip): array
     {
-        $response = $this->post('/rapid_reverse_ip', [
+        return $this->post('/rapid_reverse_ip', [
             'input' => $ip
         ]);
-        return $this->json($response);
     }
 
     public function external_ips(): array
@@ -161,115 +141,55 @@ class ApiUtils
         return $this->json($response);
     }
 
-    private function external_ips_private(): ResponseInterface
+    private function external_ips_private(): array
     {
-        // ../.. to remove /api/v1/ so go back to root.
-        return $this->get('/../../sentinel_external_ips', []);
+        // ../.. to remove /api/v1/ to go back to root
+        return $this->get('/../../sentinel_external_ips');
     }
 
-    private function matched_cves_private(): ResponseInterface
+    private function matched_cves_private(): array
     {
-        // ../.. to remove /api/v1/ so go back to root.
-        return $this->get('/../../cves-list', []);
+        // ../.. to remove /api/v1/ to go back to root
+        return $this->get('/../../cves-list');
     }
 
-    private function get($endpoint, $json): ResponseInterface
+    private function get($endpoint): array
     {
         $url = Config::get('towerify.adversarymeter.api') . $endpoint;
-        try {
-            $client = new Client([
-                RequestOptions::TIMEOUT => self::REQUEST_TIMEOUT,
-                'auth' => [
-                    config('towerify.adversarymeter.api_username'),
-                    config('towerify.adversarymeter.api_password')
-                ]
-            ]);
-            return $client->get($url, $this->httpHeaders($json));
-        } catch (ClientException $exception) {
-            throw new ApiException('API client problem (code: ' . $exception->getCode() . ', url: ' . $url . ')', $exception->getCode(), $exception);
-        } catch (Exception $exception) {
-            throw new ApiException('API problem: ' . $exception->getMessage(), $exception->getCode(), $exception);
+
+        $response = Http::withBasicAuth(
+            config('towerify.adversarymeter.api_username'),
+            config('towerify.adversarymeter.api_password')
+        )->withHeaders([
+            'Accept' => 'application/json',
+        ])->get($url);
+
+        if ($response->successful()) {
+            $json = $response->json();
+            // Log::debug($json);
+            return $json ?: [];
         }
+        Log::error($response->body());
+        return [];
     }
 
-    private function post($endpoint, $json): ResponseInterface
+    private function post($endpoint, $json): array
     {
         $url = Config::get('towerify.adversarymeter.api') . $endpoint;
-        try {
-            $client = new Client([
-                RequestOptions::TIMEOUT => self::REQUEST_TIMEOUT,
-                'auth' => [
-                    config('towerify.adversarymeter.api_username'),
-                    config('towerify.adversarymeter.api_password')
-                ]
-            ]);
-            return $client->post($url, $this->httpHeaders($json));
-        } catch (ClientException $exception) {
-            throw new ApiException('API client problem (code: ' . $exception->getCode() . ', url: ' . $url . ')', $exception->getCode(), $exception);
-        } catch (Exception $exception) {
-            throw new ApiException('API problem: ' . $exception->getMessage(), $exception->getCode(), $exception);
+
+        $response = Http::withBasicAuth(
+            config('towerify.adversarymeter.api_username'),
+            config('towerify.adversarymeter.api_password')
+        )->withHeaders([
+            'Accept' => 'application/json',
+        ])->post($url, $json);
+
+        if ($response->successful()) {
+            $json = $response->json();
+            // Log::debug($json);
+            return $json ?: [];
         }
-    }
-
-    private function httpHeaders($json): array
-    {
-        return [
-
-            'json' => $json,
-
-            // Set content type
-            'Content-Type' => 'application/json',
-
-            // No SSL verification. Guzzle try to verify even a HTTP request (so it fails)
-            // See http://docs.guzzlephp.org/en/latest/request-options.html#verify
-            'verify' => false,
-
-            // With Exception on HTTP error (400, 500, etc)
-            // See http://docs.guzzlephp.org/en/latest/request-options.html#http-errors
-            'http_errors' => true,
-        ];
-    }
-
-    private function json(ResponseInterface $response): array
-    {
-        $body = $this->body($response);
-        return $this->isOk($response, $body) ? $this->results($response, $body) : [];
-    }
-
-    private function body(ResponseInterface $response): array
-    {
-        ini_set("memory_limit", "-1");
-        if (version_compare(PHP_VERSION, '5.4.0', '>=')
-            && !(defined('JSON_C_VERSION')
-                && PHP_INT_SIZE > 4)
-        ) {
-            /** In PHP >=5.4.0, json_decode() accepts an options parameter, that allows you
-             * to specify that large ints (like Steam Transaction IDs) should be treated as
-             * strings, rather than the PHP default behaviour of converting them to floats.
-             */
-            return json_decode($response->getBody(), true, 512, JSON_BIGINT_AS_STRING);
-        }
-
-        /** Not all servers will support that, however, so for older versions we must
-         * manually detect large ints in the JSON string and quote them (thus converting
-         *them to strings) before decoding, hence the preg_replace() call.
-         */
-        $max_int_length = strlen((string)PHP_INT_MAX) - 1;
-        $json_without_bigints =
-            preg_replace('/:\s*(-?\d{' . $max_int_length . ',})/', ': "$1"', $response->getBody());
-        return json_decode($json_without_bigints, true);
-    }
-
-    private function isOk(ResponseInterface $response, array $body): bool
-    {
-        if ($response->getStatusCode() === 200) {
-            return true;
-        }
-        throw new ApiException('Fast API problem: ' . ($body['message'] ?? 'unknown'));
-    }
-
-    private function results(ResponseInterface $response, array $body): array
-    {
-        return $this->isOk($response, $body) && !empty($body) ? $body : [];
+        Log::error($response->body());
+        return [];
     }
 }
