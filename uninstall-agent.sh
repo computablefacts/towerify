843ac5407840f3f40690e4d496f9f6e8e8467be7
csavelief <csavelief@mncc.fr>
2024-12-03 13:27:12 +0100

    Fixup! Osquery - Uninstall script

--- uninstall-agent.sh
+++ uninstall-agent.sh
@@ -3,6 +3,11 @@
 systemctl stop osqueryd
 systemctl stop logalert
 
+crontab -l | grep -v "osquery" | crontab -
+crontab -l | grep -v "logparser" | crontab -
+crontab -l | grep -v "logalert" | crontab -
+crontab -l | grep -v "cywise" | crontab -
+
 apt-get purge --auto-remove osquery
 
 rm -rf /opt/osquery
