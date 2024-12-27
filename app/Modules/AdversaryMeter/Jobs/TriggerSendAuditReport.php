43d1b5cf8734c9273348b6416b00f73d264f2eda
csavelief <csavelief@mncc.fr>
2024-12-27 11:28:25 +0100

    Audit Reports - Do not send emails to debug accounts

--- app/Modules/AdversaryMeter/Jobs/TriggerSendAuditReport.php
+++ app/Modules/AdversaryMeter/Jobs/TriggerSendAuditReport.php
@@ -9,6 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Str;
 
 class TriggerSendAuditReport implements ShouldQueue
 {
@@ -25,9 +26,11 @@ class TriggerSendAuditReport implements ShouldQueue
 
     public function handle()
     {
+        $domains = collect(config('towerify.telescope.whitelist.domains'))->map(fn(string $domain) => '@' . $domain)->toArray();
         User::where('is_active', true)
             ->get()
             ->filter(fn(User $user) => $user->canUseAdversaryMeter())
+            ->filter(fn(User $user) => !Str::endsWith($user->email, $domains) || !Str::contains(Str::before($user->email, '@'), '+')) // do not send emails to debug accounts
             ->each(fn(User $user) => SendAuditReport::dispatch($user));
     }
 }
