2f4764a9d83d8632be5cb386656e8bf11247ff44
csavelief <csavelief@mncc.fr>
2025-01-22 18:57:19 +0100

    Snippet - Add comments and fix misc. bugs

--- app/Helpers/Snippet.php
+++ app/Helpers/Snippet.php
@@ -2,20 +2,36 @@
 
 namespace App\Helpers;
 
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 use InvalidArgumentException;
 
+/**
+ * Based on @{link https://boyter.org/2013/04/building-a-search-result-extract-generator-in-php/}
+ */
 class Snippet
 {
+    private const UNWANTED_CHARACTERS = array('Š' => 'S', 'š' => 's', 'Ž' => 'Z', 'ž' => 'z', 'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'A', 'Å' => 'A', 'Æ' => 'A', 'Ç' => 'C', 'È' => 'E', 'É' => 'E',
+        'Ê' => 'E', 'Ë' => 'E', 'Ì' => 'I', 'Í' => 'I', 'Î' => 'I', 'Ï' => 'I', 'Ñ' => 'N', 'Ò' => 'O', 'Ó' => 'O', 'Ô' => 'O', 'Õ' => 'O', 'Ö' => 'O', 'Ø' => 'O', 'Ù' => 'U',
+        'Ú' => 'U', 'Û' => 'U', 'Ü' => 'U', 'Ý' => 'Y', 'Þ' => 'B', 'ß' => 'Ss', 'à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a', 'ä' => 'a', 'å' => 'a', 'æ' => 'a', 'ç' => 'c',
+        'è' => 'e', 'é' => 'e', 'ê' => 'e', 'ë' => 'e', 'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i', 'ð' => 'o', 'ñ' => 'n', 'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'õ' => 'o',
+        'ö' => 'o', 'ø' => 'o', 'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ý' => 'y', 'þ' => 'b', 'ÿ' => 'y');
+
+    public static function normalize(string $string): string
+    {
+        return Str::lower(strtr($string, self::UNWANTED_CHARACTERS));
+    }
+
     /**
-     * Extract a snippet with default parameters.
+     * 1/6 ratio on prevCount tends to work pretty well and puts the terms in the middle of the extract.
      *
      * @param array $words List of words around which to center the snippet.
      * @param string $text Text to snippet.
-     * @return string The snippet.
+     * @return Collection The snippets.
      */
-    public static function extract(array $words, string $text): string
+    public static function extract(array $words, string $text, bool $and = false): Collection
     {
-        return self::extractEx($words, $text, 300, 50, '...');
+        return self::extractEx($words, $text, 300, 50, '...', $and);
     }
 
     /**
@@ -26,9 +42,9 @@ class Snippet
      * @param int $relLength Maximum length of the snippet in characters.
      * @param int $prevCount Number of characters to display before the leftmost match.
      * @param string|null $indicator Ellipsis to indicate where the text has been trimmed.
-     * @return string The snippet.
+     * @return Collection The snippets.
      */
-    public static function extractEx(array $words, string $text, int $relLength, int $prevCount, ?string $indicator = '...'): string
+    public static function extractEx(array $words, string $text, int $relLength, int $prevCount, ?string $indicator = '...', bool $and = false): Collection
     {
         if (empty($words)) {
             throw new InvalidArgumentException("words array cannot be empty");
@@ -37,31 +53,46 @@ class Snippet
             throw new InvalidArgumentException("relLength and prevCount must be >= 0");
         }
 
-        $indicator = $indicator ?? '...';
-        $textLength = strlen($text);
-
-        if ($textLength <= $relLength) {
-            return $text;
-        }
-
+        $indicator = $indicator ?? '...'; // for backward compatibility
+        $textLength = Str::length($text);
         $locations = self::wordsLocations(array_unique($words), $text);
 
-        $startPos = !empty($locations) ? self::snippetLocation($locations, $relLength, $prevCount) : 0;
+        if (count($locations) === 0) {
+            return collect();
+        }
+        if ($and) {
+            $matched = array_unique(array_map(fn(array $location) => $location['text'], $locations));
+            if (count($matched) < count($words)) {
+                return collect();
+            }
+        }
+        if ($textLength <= $relLength) {
+            return collect([$text]);
+        }
+
+        $startPos = self::snippetLocation($locations, $relLength, $prevCount);
         $begin = $startPos;
         $end = min($textLength, $startPos + $relLength);
 
+        // If we are going to snip too much...
         if (($begin + $relLength) > $textLength) {
             $begin = max(0, $textLength - $relLength);
         }
+
+        // Check to ensure we dont snip the first word
         if ($begin > 0) {
-            $index = strrpos(substr($text, 0, $begin), ' ');
+            $index = self::lastIndexOf($text, ' ', $begin);
             $begin = $index === false ? max(0, $begin - $prevCount) : $index + 1;
         }
+
+        // Check to ensure we don't snip the last word
         if (($begin + $relLength) < $textLength) {
-            $index = strpos($text, ' ', $end);
+            $index = Str::position($text, ' ', $end);
             $end = $index === false ? min($textLength, $end + $prevCount) : $index;
         }
-        return ($begin > 0 ? $indicator : "") . substr($text, $begin, $end - $begin) . ($end < $textLength ? $indicator : "");
+
+        // If we trimmed from the begin/end add ellipsis
+        return collect([($begin > 0 ? $indicator : "") . Str::substr($text, $begin, $end - $begin) . ($end < $textLength ? $indicator : "")]);
     }
 
     /**
@@ -71,22 +102,24 @@ class Snippet
      * @param string $text The text to search.
      * @return array List of word spans (start, end).
      */
-    private static function wordsLocations(array $words, string $text): array
+    public static function wordsLocations(array $words, string $text): array
     {
         $locations = [];
 
         foreach ($words as $word) {
-            $length = strlen($word);
-            $loc = strpos($text, $word);
+
+            $length = Str::length($word);
+            $loc = Str::position($text, $word);
 
             while ($loc !== false) {
-                $precedingChar = $loc === 0 ? null : $text[$loc - 1];
+
+                $precedingChar = $loc === 0 ? null : Str::charAt($text, $loc - 1);
 
                 if ($loc === 0 || !ctype_alnum($precedingChar)) {
                     $locations[] = ['begin' => $loc, 'text' => $word, 'end' => $loc + $length];
                 }
 
-                $loc = strpos($text, $word, $loc + $length);
+                $loc = Str::position($text, $word, $loc + $length);
             }
         }
         usort($locations, function ($a, $b) {
@@ -115,23 +148,46 @@ class Snippet
 
         for ($i = 0; $i < count($locations); $i++) {
 
-            $endPos = $locations[$i]['begin'] + strlen($locations[$i]['text']);
+            $endPos = $locations[$i]['begin'] + Str::length($locations[$i]['text']);
             $beginPos = $locations[$i]['begin'];
             $words = [$locations[$i]['text']];
 
             for ($j = $i + 1; $j < count($locations) && ($locations[$j]['begin'] - $beginPos) < $relLength; $j++) {
-                $endPos = $locations[$j]['begin'] + strlen($locations[$j]['text']);
+                $endPos = $locations[$j]['begin'] + Str::length($locations[$j]['text']);
                 $words[] = $locations[$j]['text'];
             }
 
             $distinctWordCount = count(array_unique($words));
 
-            if ($distinctWordCount > $nbDistinctWords || ($distinctWordCount == $nbDistinctWords && $endPos - $beginPos < $bestDiff)) {
+            if ($distinctWordCount > $nbDistinctWords /* maximize the number of distinct words */ ||
+                ($distinctWordCount === $nbDistinctWords && $endPos - $beginPos < $bestDiff) /* minimize the window size */) {
                 $bestLocation = $beginPos;
                 $bestDiff = $endPos - $beginPos;
                 $nbDistinctWords = $distinctWordCount;
             }
         }
         return max(0, $bestLocation - $prevCount);
+    }
+
+    /**
+     * Finds the last index of a given substring in a string, starting from a given offset.
+     *
+     * @param string $text The string to search in.
+     * @param string $needle The substring to search for.
+     * @param int|null $offset The position (exclusive) from which to search backward. Optional.
+     * @return int|bool The position of the last occurrence of the substring, or false if not found.
+     */
+    private static function lastIndexOf(string $text, string $needle, ?int $offset = null)
+    {
+        // If no offset is given, start from the end of the string
+        $offset = $offset ?? Str::length($text);
+
+        // Extract substring up to the given offset
+        $substring = Str::substr($text, 0, $offset);
+
+        // Find the last occurrence of the needle in the substring
+        $lastPosition = Str::length(Str::beforeLast($substring, $needle));
+
+        return $lastPosition === Str::length($substring) ? false : $lastPosition; // false if not found
     }
 }
--- tests/Unit/SnippetTest.php
+++ tests/Unit/SnippetTest.php
@@ -9,86 +9,86 @@ class SnippetTest extends TestCaseNoDb
 {
     public function testSearchedWordsNotInText()
     {
-        $snippet = Snippet::extract(["not_here"], "Hello world!");
-        $this->assertEquals("Hello world!", $snippet);
+        $snippets = Snippet::extract(["not_here"], "Hello world!");
+        $this->assertTrue($snippets->isEmpty());
     }
 
     public function testTextShorterThanSnippetMaxLength()
     {
-        $snippet = Snippet::extract(["world"], "Hello world!");
-        $this->assertEquals("Hello world!", $snippet);
+        $snippets = Snippet::extract(["world"], "Hello world!");
+        $this->assertEquals("Hello world!", $snippets->first());
     }
 
     public function testSnippetWithLeftEllipsis()
     {
-        $snippet = Snippet::extract(["Yahoo", "Outlook"], $this->text());
-        $this->assertEquals("...in-the-know with the latest news and information. CloudSponge provides an interface to easily enable your users to import contacts from a variety of the most popular webmail services including Yahoo, Gmail and Hotmail/MSN as well as popular desktop address books such as Mac Address Book and Outlook.", $snippet);
+        $snippets = Snippet::extract(["Yahoo", "Outlook"], $this->text());
+        $this->assertEquals("...in-the-know with the latest news and information. CloudSponge provides an interface to easily enable your users to import contacts from a variety of the most popular webmail services including Yahoo, Gmail and Hotmail/MSN as well as popular desktop address books such as Mac Address Book and Outlook.", $snippets->first());
     }
 
     public function testSnippetWithoutLeftEllipsis()
     {
-        $snippet = Snippet::extractEx(["Yahoo", "Outlook"], $this->text(), 300, 50, "");
-        $this->assertEquals("in-the-know with the latest news and information. CloudSponge provides an interface to easily enable your users to import contacts from a variety of the most popular webmail services including Yahoo, Gmail and Hotmail/MSN as well as popular desktop address books such as Mac Address Book and Outlook.", $snippet);
+        $snippets = Snippet::extractEx(["Yahoo", "Outlook"], $this->text(), 300, 50, "");
+        $this->assertEquals("in-the-know with the latest news and information. CloudSponge provides an interface to easily enable your users to import contacts from a variety of the most popular webmail services including Yahoo, Gmail and Hotmail/MSN as well as popular desktop address books such as Mac Address Book and Outlook.", $snippets->first());
     }
 
     public function testSnippetWithRightEllipsis()
     {
-        $snippet = Snippet::extract(["most", "visited", "home", "page"], $this->text());
-        $this->assertEquals("Welcome to Yahoo!, the world’s most visited home page. Quickly find what you’re searching for, get in touch with friends and stay in-the-know with the latest news and information. CloudSponge provides an interface to easily enable your users to import contacts from a variety of the most popular webmail...", $snippet);
+        $snippets = Snippet::extract(["most", "visited", "home", "page"], $this->text());
+        $this->assertEquals("Welcome to Yahoo!, the world’s most visited home page. Quickly find what you’re searching for, get in touch with friends and stay in-the-know with the latest news and information. CloudSponge provides an interface to easily enable your users to import contacts from a variety of the most popular webmail...", $snippets->first());
     }
 
     public function testSnippetWithLeftAndRightEllipsis()
     {
-        $snippet = Snippet::extract(["latest", "news", "CloudSponge"], $this->text());
-        $this->assertEquals("...in touch with friends and stay in-the-know with the latest news and information. CloudSponge provides an interface to easily enable your users to import contacts from a variety of the most popular webmail services including Yahoo, Gmail and Hotmail/MSN as well as popular desktop address books such as Mac...", $snippet);
+        $snippets = Snippet::extract(["latest", "news", "CloudSponge"], $this->text());
+        $this->assertEquals("...in touch with friends and stay in-the-know with the latest news and information. CloudSponge provides an interface to easily enable your users to import contacts from a variety of the most popular webmail services including Yahoo, Gmail and Hotmail/MSN as well as popular desktop address books such as Mac...", $snippets->first());
     }
 
     public function testSnippetWithEmptyIndicator()
     {
-        $snippet = Snippet::extractEx(["latest", "news", "CloudSponge"], $this->text(), 300, 50, "");
-        $this->assertEquals("in touch with friends and stay in-the-know with the latest news and information. CloudSponge provides an interface to easily enable your users to import contacts from a variety of the most popular webmail services including Yahoo, Gmail and Hotmail/MSN as well as popular desktop address books such as Mac", $snippet);
+        $snippets = Snippet::extractEx(["latest", "news", "CloudSponge"], $this->text(), 300, 50, "");
+        $this->assertEquals("in touch with friends and stay in-the-know with the latest news and information. CloudSponge provides an interface to easily enable your users to import contacts from a variety of the most popular webmail services including Yahoo, Gmail and Hotmail/MSN as well as popular desktop address books such as Mac", $snippets->first());
     }
 
     public function testSnippetWithNullIndicator()
     {
-        $snippet = Snippet::extractEx(["latest", "news", "CloudSponge"], $this->text(), 300, 50, null);
-        $this->assertEquals("...in touch with friends and stay in-the-know with the latest news and information. CloudSponge provides an interface to easily enable your users to import contacts from a variety of the most popular webmail services including Yahoo, Gmail and Hotmail/MSN as well as popular desktop address books such as Mac...", $snippet);
+        $snippets = Snippet::extractEx(["latest", "news", "CloudSponge"], $this->text(), 300, 50, null);
+        $this->assertEquals("...in touch with friends and stay in-the-know with the latest news and information. CloudSponge provides an interface to easily enable your users to import contacts from a variety of the most popular webmail services including Yahoo, Gmail and Hotmail/MSN as well as popular desktop address books such as Mac...", $snippets->first());
     }
 
     public function testDoNotTruncateHead()
     {
-        $snippet = Snippet::extractEx(["gmail"], "zor@gmail.com", 5, 0, "...");
-        $this->assertEquals("...gmail...", $snippet);
+        $snippets = Snippet::extractEx(["gmail"], "zor@gmail.com", 5, 0, "...");
+        $this->assertEquals("...gmail...", $snippets->first());
     }
 
     public function testDoNotTruncateHeadWithNullIndicator()
     {
-        $snippet = Snippet::extractEx(["gmail"], "zor@gmail.com", 5, 0, null);
-        $this->assertEquals("...gmail...", $snippet);
+        $snippets = Snippet::extractEx(["gmail"], "zor@gmail.com", 5, 0, null);
+        $this->assertEquals("...gmail...", $snippets->first());
     }
 
     public function testDoNotTruncateHeadWithEmptyIndicator()
     {
-        $snippet = Snippet::extractEx(["gmail"], "zor@gmail.com", 5, 0, "");
-        $this->assertEquals("gmail", $snippet);
+        $snippets = Snippet::extractEx(["gmail"], "zor@gmail.com", 5, 0, "");
+        $this->assertEquals("gmail", $snippets->first());
     }
 
     public function testDoNotTruncateTail()
     {
-        $snippet = Snippet::extractEx(["zor"], "zor@gmail.com", 3, 50, "...");
-        $this->assertEquals("zor@gmail.com", $snippet);
+        $snippets = Snippet::extractEx(["zor"], "zor@gmail.com", 3, 50, "...");
+        $this->assertEquals("zor@gmail.com", $snippets->first());
     }
 
     public function testDoNotTruncateTailWithNullIndicator()
     {
-        $snippet = Snippet::extractEx(["zor"], "zor@gmail.com", 3, 50, null);
-        $this->assertEquals("zor@gmail.com", $snippet);
+        $snippets = Snippet::extractEx(["zor"], "zor@gmail.com", 3, 50, null);
+        $this->assertEquals("zor@gmail.com", $snippets->first());
     }
 
     public function testDoNotTruncateTailWithEmptyIndicator()
     {
-        $snippet = Snippet::extractEx(["zor"], "zor@gmail.com", 3, 50, "");
-        $this->assertEquals("zor@gmail.com", $snippet);
+        $snippets = Snippet::extractEx(["zor"], "zor@gmail.com", 3, 50, "");
+        $this->assertEquals("zor@gmail.com", $snippets->first());
     }
 
     private function text(): string
