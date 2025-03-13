3866d64ac45fc55f772693038ff1ae80a03b5f02
Patrick Brisacier <pbrisacier@mncc.fr>
2025-03-13 11:54:30 +0100

    Table: change UpdateTables job to support Azure tables

--- app/Modules/CyberBuddy/Jobs/UpdateTables.php
+++ app/Modules/CyberBuddy/Jobs/UpdateTables.php
@@ -4,6 +4,7 @@ namespace App\Modules\CyberBuddy\Jobs;
 
 use App\Modules\CyberBuddy\Events\ImportTable;
 use App\Modules\CyberBuddy\Helpers\ClickhouseUtils;
+use App\Modules\CyberBuddy\Helpers\TableStorage;
 use App\Modules\CyberBuddy\Models\Table;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -32,46 +33,26 @@ class UpdateTables implements ShouldQueue
             ->get()
             ->each(function (Table $table) {
 
-                Auth::loginUsingId($table->id);
+                Auth::loginUsingId($table->created_by);
 
                 $user = Auth::user();
-                $region = $table->credentials['region'];
-                $accessKeyId = $table->credentials['access_key_id'];
-                $secretAccessKey = $table->credentials['secret_access_key'];
-                $inputFolder = $table->credentials['input_folder'];
-                $outputFolder = $table->credentials['output_folder'];
 
-                $s3Client = new \Aws\S3\S3Client([
-                    'region' => $region,
-                    'version' => 'latest',
-                    'credentials' => [
-                        'key' => $accessKeyId,
-                        'secret' => $secretAccessKey,
-                    ],
-                ]);
+                $disk = TableStorage::inDisk($table->credentials);
+                collect($disk->files())->filter(function ($file) use ($table) {
+                    return ClickhouseUtils::normalizeTableName($file) === $table->name;
+                })->filter(function ($file) use ($disk, $table) {
+                    return (!$table->finished_at || Carbon::createFromTimestamp($disk->lastModified($file))->isAfter($table->finished_at));
+                })->each(function ($file) use ($user, $table) {
 
-                $bucket = explode('/', $inputFolder, 2)[0];
-                $prefix = isset(explode('/', $inputFolder, 2)[1]) ? explode('/', $inputFolder, 2)[1] : '';
-                $objects = $s3Client->listObjectsV2([
-                    'Bucket' => $bucket,
-                    'Prefix' => $prefix,
-                ]);
+                    $credentials = $table->credentials;
+                    $copy = $table->copied;
+                    $deduplicate = $table->deduplicated;
+                    $updatable = $table->updatable;
+                    $description = $table->description;
+                    $columns = $table->schema;
 
-                collect($objects['Contents'] ?? [])
-                    ->filter(function (array $object) use ($table) {
-                        return ClickhouseUtils::normalizeTableName($object['Key']) === $table->name &&
-                            (!$table->finished_at || Carbon::createFromTimestamp($object['LastModified']->getTimestamp())->isAfter($table->finished_at));
-                    })
-                    ->each(function (array $object) use ($user, $region, $accessKeyId, $secretAccessKey, $inputFolder, $outputFolder, $table) {
-
-                        $copy = $table->copied;
-                        $deduplicate = $table->deduplicated;
-                        $updatable = $table->updatable;
-                        $description = $table->description;
-                        $columns = $table->schema;
-
-                        ImportTable::dispatch($user, $region, $accessKeyId, $secretAccessKey, $inputFolder, $outputFolder, $copy, $deduplicate, $updatable, $object['Key'], $columns, $description);
-                    });
+                    ImportTable::dispatch($user, $credentials, $copy, $deduplicate, $updatable, $file, $columns, $description);
+                });
             });
 
         // TODO : update dependencies
