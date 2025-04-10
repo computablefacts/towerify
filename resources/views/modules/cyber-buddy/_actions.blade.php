9b5fe7407c101c92b9252342fe4153bf18018b9d
csavelief <csavelief@mncc.fr>
2025-04-10 14:45:41 +0200

    Fix actions hyperlinks

--- resources/views/modules/cyber-buddy/_actions.blade.php
+++ resources/views/modules/cyber-buddy/_actions.blade.php
@@ -28,26 +28,40 @@
 <div class="tw-actions" style="display: none;">
   <div class="tw-centered">
     <div class="tw-grid">
+      @php
+
+      $urlCharteInformatique = route('home', ['tab' => 'ai_writer']);
+      $charteInformatique = "Vous souhaitez créer ou importer une Charte Informatique? Cliquez <a href=\"$urlCharteInformatique\">ici</a>!";
+
+      $urlPssi = route('home', ['tab' => 'ai_writer']);
+      $pssi = "Vous souhaitez créer ou importer une Politique de Sécurité des Systèmes d\'Information? Cliquez <a href=\"$urlPssi\">ici</a>!";
+
+      $urlHoneypots = App\Modules\AdversaryMeter\Helpers\AdversaryMeter::redirectUrl('setup_honeypots');
+      $honeypots = "Vous souhaitez piéger les acteurs malveillants? Cliquez <a href=\"$urlHoneypots\">ici</a>!";
+
+      $urlScanner = App\Modules\AdversaryMeter\Helpers\AdversaryMeter::redirectUrl('assets');
+      $scanner = "Vous souhaitez surveiller vos serveurs exposés sur Internet? Cliquez <a href=\"$urlScanner\">ici</a>!";
+
+      @endphp
       @include('modules.cyber-buddy._action', [
       'title' => 'Charte Informatique',
       'subtitle' => 'Définissez les règles d\'utilisation de vos systèmes',
-      'text' => 'Vous souhaitez créer ou importer une Charte Informatique? Cliquez <a href="">ici</a>!'
+      'text' => $charteInformatique
       ])
       @include('modules.cyber-buddy._action', [
       'title' => 'PSSI',
       'subtitle' => 'Définissez la politique de sécurité de vos systèmes',
-      'text' => 'Vous souhaitez créer ou importer une Politique de Sécurité des Systèmes d\'Information? Cliquez <a
-        href="">ici</a>!'
+      'text' => $pssi
       ])
       @include('modules.cyber-buddy._action', [
       'title' => 'Honeypots',
       'subtitle' => 'Evaluez l\'état des menaces qui vous ciblent',
-      'text' => 'Vous souhaitez piéger les acteurs malveillants? Cliquez <a href="">ici</a>!'
+      'text' => $honeypots
       ])
       @include('modules.cyber-buddy._action', [
       'title' => 'Scanner de vulnérabilités',
       'subtitle' => 'Testez votre infrastructure',
-      'text' => 'Vous souhaitez surveiller vos serveurs exposés sur Internet? Cliquez <a href="">ici</a>!'
+      'text' => $scanner
       ])
     </div>
   </div>
