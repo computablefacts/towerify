a63c06ff5f1e26aba90274f804e06e306b9d1893
csavelief <csavelief@mncc.fr>
2025-04-28 16:46:18 +0200

    Rename migration

--- database/migrations/2025_04_25_170300_index_column_asset_in_table_assets_2.php
+++ database/migrations/2025_04_25_170300_index_column_asset_in_table_assets_2.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class IndexColumnAssetInTableAssets extends Migration
+class IndexColumnAssetInTableAssets2 extends Migration
 {
     /**
      * Run the migrations.
@@ -14,8 +14,16 @@ class IndexColumnAssetInTableAssets extends Migration
     public function up()
     {
         Schema::table('am_assets', function (Blueprint $table) {
-            $table->dropUnique(['asset', 'created_by']);
-            $table->unique(['asset', 'created_by', 'ynh_trial_id']);
+            try {
+                $table->dropUnique(['asset', 'created_by']);
+            } catch (\Exception $e) {
+                \Illuminate\Support\Facades\Log::warning($e->getMessage());
+            }
+            try {
+                $table->unique(['asset', 'created_by', 'ynh_trial_id']);
+            } catch (\Exception $e) {
+                \Illuminate\Support\Facades\Log::warning($e->getMessage());
+            }
         });
     }
 
