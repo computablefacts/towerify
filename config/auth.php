5fdf468c17147e36d847461f069ffdb4d4711de8
csavelief <csavelief@mncc.fr>
2025-05-16 12:03:12 +0200

    Increase the reset token expiration date to 15 jours

--- config/auth.php
+++ config/auth.php
@@ -95,7 +95,7 @@ return [
         'users' => [
             'provider' => 'users',
             'table' => 'password_resets',
-            'expire' => 90,
+            'expire' => 21600, // 15 jours
         ],
     ],
 
