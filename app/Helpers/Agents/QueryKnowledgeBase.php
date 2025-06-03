41c381963f6ed0cc06b5d6ffc547f0fd54dd51e1
csavelief <csavelief@mncc.fr>
2025-06-03 13:54:26 +0200

    Improve prompts

--- app/Helpers/Agents/QueryKnowledgeBase.php
+++ app/Helpers/Agents/QueryKnowledgeBase.php
@@ -283,10 +283,10 @@ class QueryKnowledgeBase extends AbstractAction
         $prompt->template = Str::replace('{MEMOS}', $memos, $prompt->template);
         $prompt->template = Str::replace('{NOTES}', $notes, $prompt->template);
         $prompt->template = Str::replace('{QUESTION}', $json['question_en'], $prompt->template);
-        // Log::debug($prompt->template);
+        Log::debug($prompt->template);
         $response = DeepInfra::execute($prompt->template, 'Qwen/Qwen3-30B-A3B');
         $answer = $response['choices'][0]['message']['content'] ?? '';
-        // Log::debug("[3] answer : {$answer}");
+        Log::debug("[3] answer : {$answer}");
         return preg_replace('/<think>.*?<\/think>/s', '', $answer);
     }
 }
