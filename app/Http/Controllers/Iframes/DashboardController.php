7a780f67d330ded573bf5ab975344e9d5bec3894
csavelief <csavelief@mncc.fr>
2025-06-11 21:51:58 +0200

    Move honeypot cards to the Dashboard

--- app/Http/Controllers/Iframes/DashboardController.php
+++ app/Http/Controllers/Iframes/DashboardController.php
@@ -6,7 +6,11 @@ use App\Http\Controllers\Controller;
 use App\Http\Procedures\AssetsProcedure;
 use App\Http\Procedures\VulnerabilitiesProcedure;
 use App\Models\Alert;
+use App\Models\Honeypot;
+use App\Models\HoneypotEvent;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 
 class DashboardController extends Controller
@@ -27,6 +31,7 @@ class DashboardController extends Controller
         $nbHigh = count($alerts['high'] ?? []);
         $nbMedium = count($alerts['medium'] ?? []);
         $nbLow = count($alerts['low'] ?? []);
+        
         $todo = collect($alerts['high'] ?? [])
             ->concat($alerts['medium'] ?? [])
             ->concat($alerts['low'] ?? [])
@@ -45,6 +50,37 @@ class DashboardController extends Controller
             ->values()
             ->take(5);
 
+        $honeypots = Honeypot::all()
+            ->map(function (Honeypot $honeypot) {
+                $counts = $this->honeypotEventCounts($honeypot);
+                $max = collect($counts)->max(fn($count) => $count['human_or_targeted'] + $count['not_human_or_targeted']);
+                $sum = collect($counts)->sum(fn($count) => $count['human_or_targeted'] + $count['not_human_or_targeted']);
+                return [
+                    'name' => $honeypot->dns,
+                    'type' => $honeypot->cloud_sensor,
+                    'counts' => $counts,
+                    'max' => $max,
+                    'sum' => $sum,
+                ];
+            })
+            ->sortBy(fn(array $honeypot) => [-$honeypot['sum'], $honeypot['name']])
+            ->values()
+            ->take(3)
+            ->toArray();
+
+        $mostRecentHoneypotEvents = Honeypot::all()
+            ->map(function (Honeypot $honeypot) {
+                $events = $this->mostRecentHoneypotEvents($honeypot);
+                return [
+                    'name' => $honeypot->dns,
+                    'events' => $events,
+                ];
+            })
+            ->groupBy('name')
+            ->map(fn($group) => $group->first())
+            ->take(3)
+            ->toArray();
+
         return view('cywise.iframes.dashboard', [
             'nb_monitored' => $nbMonitored,
             'nb_monitorable' => $nbMonitorable,
@@ -52,6 +88,54 @@ class DashboardController extends Controller
             'nb_medium' => $nbMedium,
             'nb_low' => $nbLow,
             'todo' => $todo,
+            'honeypots' => $honeypots,
+            'most_recent_honeypot_events' => $mostRecentHoneypotEvents,
         ]);
+    }
+
+    private function honeypotEventCounts(Honeypot $honeypot): array
+    {
+        $cutOffTime = Carbon::now()->startOfDay()->subMonth();
+        return HoneypotEvent::select(
+            DB::raw("DATE_FORMAT(timestamp, '%Y-%m-%d') AS date"),
+            DB::raw("SUM(CASE WHEN human = 1 OR targeted = 1 THEN 1 ELSE 0 END) AS human_or_targeted"),
+            DB::raw("SUM(CASE WHEN human = 0 AND targeted = 0 THEN 1 ELSE 0 END) AS not_human_or_targeted")
+        )
+            ->where('timestamp', '>=', $cutOffTime)
+            ->where('honeypot_id', $honeypot->id)
+            ->groupBy('date')
+            ->orderBy('date', 'desc') // keep only the most recent ones
+            ->limit(10)
+            ->get()
+            ->sortBy('date') // most recent date at the end
+            ->toArray();
+    }
+
+    private function mostRecentHoneypotEvents(Honeypot $honeypot): array
+    {
+        /** @var array $ips */
+        $ips = config('towerify.adversarymeter.ip_addresses');
+        return HoneypotEvent::select(
+            'am_honeypots_events.*',
+            DB::raw("CASE WHEN am_attackers.name IS NULL THEN '-' ELSE am_attackers.name END AS internal_name"),
+            DB::raw("CASE WHEN am_attackers.id IS NULL THEN '-' ELSE am_attackers.id END AS attacker_id"),
+        )
+            ->where('honeypot_id', $honeypot->id)
+            ->whereNotIn('ip', $ips)
+            ->leftJoin('am_attackers', 'am_attackers.id', '=', 'am_honeypots_events.attacker_id')
+            ->orderBy('timestamp', 'desc')
+            ->limit(5)
+            ->get()
+            ->map(function (HoneypotEvent $event) {
+                return [
+                    'timestamp' => $event->timestamp->utc()->format('Y-m-d H:i:s'),
+                    'event_type' => $event->event,
+                    'event_details' => $event->details,
+                    'attacker_ip' => $event->ip,
+                    'attacker_name' => $event->internal_name,
+                    'attacker_id' => $event->attacker_id,
+                ];
+            })
+            ->toArray();
     }
 }
--- resources/views/cywise/iframes/dashboard.blade.php
+++ resources/views/cywise/iframes/dashboard.blade.php
@@ -1,169 +1,25 @@
 @extends('cywise.iframes.app')
 
 @section('content')
-<!-- ASSETS -->
 <div class="row pt-3">
-  <div class="col-4">
-    <div class="card">
-      <div class="card-body p-3">
-        <div class="row align-items-center">
-          <div class="col-auto">
-            <div class="d-flex align-content-center">
-              <span class="bg-blue text-white avatar">
-                <span class="bp4-icon bp4-icon-globe-network"></span>
-              </span>
-            </div>
-          </div>
-          <div class="col">
-            <div class="h5 mb-0">
-              <b>{{ $nb_monitored + $nb_monitorable }}</b>
-            </div>
-            <div class="text-muted">
-              {{ __('Assets') }}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="col-4 ps-0">
-    <div class="card">
-      <div class="card-body p-3">
-        <div class="row align-items-center">
-          <div class="col-auto">
-            <div class="d-flex align-content-center">
-              <span class="bg-blue text-white avatar">
-                <span class="bp4-icon bp4-icon-globe-network"></span>
-              </span>
-            </div>
-          </div>
-          <div class="col">
-            <div class="h5 mb-0">
-              <b>{{ $nb_monitored }}</b>
-            </div>
-            <div class="text-muted">
-              {{ __('Assets Monitored') }}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="col-4 ps-0">
-    <div class="card">
-      <div class="card-body p-3">
-        <div class="row align-items-center">
-          <div class="col-auto">
-            <div class="d-flex align-content-center">
-              <span class="bg-blue text-white avatar">
-                <span class="bp4-icon bp4-icon-globe-network"></span>
-              </span>
-            </div>
-          </div>
-          <div class="col">
-            <div class="h5 mb-0">
-              <b>{{ $nb_monitorable }}</b>
-            </div>
-            <div class="text-muted">
-              {{ __('Assets Monitorable') }}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-<!-- VULNERABILITIES -->
-<div class="row pt-3">
-  <div class="col-4">
-    <div class="card">
-      <div class="card-body p-3">
-        <div class="row align-items-center">
-          <div class="col-auto">
-            <div class="d-flex align-content-center">
-              <span class="bg-red text-white avatar">
-                <span class="bp4-icon bp4-icon-issue"></span>
-              </span>
-            </div>
-          </div>
-          <div class="col">
-            <div class="h5 mb-0">
-              <b>{{ $nb_high }}</b>
-            </div>
-            <div class="text-muted">
-              {{ __('Vuln. High') }}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="col-4 ps-0">
-    <div class="card">
-      <div class="card-body p-3">
-        <div class="row align-items-center">
-          <div class="col-auto">
-            <div class="d-flex align-content-center">
-              <span class="bg-orange text-white avatar">
-                <span class="bp4-icon bp4-icon-issue"></span>
-              </span>
-            </div>
-          </div>
-          <div class="col">
-            <div class="h5 mb-0">
-              <b>{{ $nb_medium }}</b>
-            </div>
-            <div class="text-muted">
-              {{ __('Vuln. Medium') }}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="col-4 ps-0">
-    <div class="card">
-      <div class="card-body p-3">
-        <div class="row align-items-center">
-          <div class="col-auto">
-            <div class="d-flex align-content-center">
-              <span class="bg-green text-white avatar">
-                <span class="bp4-icon bp4-icon-issue"></span>
-              </span>
-            </div>
-          </div>
-          <div class="col">
-            <div class="h5 mb-0">
-              <b>{{ $nb_low }}</b>
-            </div>
-            <div class="text-muted">
-              {{ __('Vuln. Low') }}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-<!-- CYBERTODO -->
-@if(count($todo) > 0)
-<style>
+  <!-- CYBERTODO : BEGIN -->
+  @if(count($todo) > 0)
+  <style>
 
-  .todo-item a:hover, .todo-item a:focus {
-    outline: 0;
-    color: var(--c-blue-500);
-  }
+    .todo-item a:hover, .todo-item a:focus {
+      outline: 0;
+      color: var(--c-blue-500);
+    }
 
-  .todo-item a {
-    color: var(--c-grey-500);
-    font-weight: 500;
-    text-decoration: none;
-    border-bottom: 1px dashed;
-  }
+    .todo-item a {
+      color: var(--c-grey-500);
+      font-weight: 500;
+      text-decoration: none;
+      border-bottom: 1px dashed;
+    }
 
-</style>
-<div class="row pt-3">
-  <div class="col-4">
+  </style>
+  <div class="col-4 pe-0">
     <div class="card">
       <div class="card-body">
         <h6 class="card-title">
@@ -197,18 +53,258 @@
       </div>
     </div>
   </div>
+  @endif
+  <!-- CYBERTODO : END -->
+  <div class="col">
+    <!-- ASSETS : BEGIN -->
+    <div class="row">
+      <div class="col">
+        <div class="card">
+          <div class="card-body p-3">
+            <div class="row align-items-center">
+              <div class="col-auto">
+                <div class="d-flex align-content-center">
+                  <span class="bg-blue text-white avatar">
+                    <span class="bp4-icon bp4-icon-globe-network"></span>
+                  </span>
+                </div>
+              </div>
+              <div class="col">
+                <div class="h5 mb-0">
+                  <b>{{ $nb_monitored + $nb_monitorable }}</b>
+                </div>
+                <div class="text-muted">
+                  {{ __('Assets') }}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col ps-0">
+        <div class="card">
+          <div class="card-body p-3">
+            <div class="row align-items-center">
+              <div class="col-auto">
+                <div class="d-flex align-content-center">
+                  <span class="bg-blue text-white avatar">
+                    <span class="bp4-icon bp4-icon-globe-network"></span>
+                  </span>
+                </div>
+              </div>
+              <div class="col">
+                <div class="h5 mb-0">
+                  <b>{{ $nb_monitored }}</b>
+                </div>
+                <div class="text-muted">
+                  {{ __('Assets Monitored') }}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col ps-0">
+        <div class="card">
+          <div class="card-body p-3">
+            <div class="row align-items-center">
+              <div class="col-auto">
+                <div class="d-flex align-content-center">
+                  <span class="bg-blue text-white avatar">
+                    <span class="bp4-icon bp4-icon-globe-network"></span>
+                  </span>
+                </div>
+              </div>
+              <div class="col">
+                <div class="h5 mb-0">
+                  <b>{{ $nb_monitorable }}</b>
+                </div>
+                <div class="text-muted">
+                  {{ __('Assets Monitorable') }}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- ASSETS : END -->
+    <!-- VULNERABILITIES : BEGIN -->
+    <div class="row pt-3">
+      <div class="col">
+        <div class="card">
+          <div class="card-body p-3">
+            <div class="row align-items-center">
+              <div class="col-auto">
+                <div class="d-flex align-content-center">
+              <span class="bg-red text-white avatar">
+                <span class="bp4-icon bp4-icon-issue"></span>
+              </span>
+                </div>
+              </div>
+              <div class="col">
+                <div class="h5 mb-0">
+                  <b>{{ $nb_high }}</b>
+                </div>
+                <div class="text-muted">
+                  {{ __('Vuln. High') }}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col ps-0">
+        <div class="card">
+          <div class="card-body p-3">
+            <div class="row align-items-center">
+              <div class="col-auto">
+                <div class="d-flex align-content-center">
+              <span class="bg-orange text-white avatar">
+                <span class="bp4-icon bp4-icon-issue"></span>
+              </span>
+                </div>
+              </div>
+              <div class="col">
+                <div class="h5 mb-0">
+                  <b>{{ $nb_medium }}</b>
+                </div>
+                <div class="text-muted">
+                  {{ __('Vuln. Medium') }}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col ps-0">
+        <div class="card">
+          <div class="card-body p-3">
+            <div class="row align-items-center">
+              <div class="col-auto">
+                <div class="d-flex align-content-center">
+              <span class="bg-green text-white avatar">
+                <span class="bp4-icon bp4-icon-issue"></span>
+              </span>
+                </div>
+              </div>
+              <div class="col">
+                <div class="h5 mb-0">
+                  <b>{{ $nb_low }}</b>
+                </div>
+                <div class="text-muted">
+                  {{ __('Vuln. Low') }}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- VULNERABILITIES : END -->
+  </div>
+</div>
+<!-- HONEYPOTS : BEGIN -->
+@if(count($honeypots) > 0)
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/charts.css/dist/charts.min.css">
+<div class="row pt-3 pe-3">
+  @foreach($honeypots as $honeypot)
+  <div class="col pe-0">
+    <div class="card">
+      <div class="card-body">
+        <h6 class="card-title text-truncate">
+          {{ $honeypot['type'] }}&nbsp;<span style="color: var(--c-orange-light);">/</span>&nbsp;{{ $honeypot['name'] }}
+        </h6>
+        @if(\Illuminate\Support\Str::endsWith($honeypot['name'], '.cywise.io'))
+        <p>{{ __('Would you like to redirect one of your domains to this honeypot? Contact support!') }}</p>
+        @endif
+        @if(count($honeypot['counts']) <= 0)
+        <p>{{ __('No recent events.') }}</p>
+        @else
+        <div class="card-text mb-3">
+          <table
+            class="charts-css column hide-data show-labels show-primary-axis show-3-secondary-axes data-spacing-3 multiple stacked">
+            <thead>
+            <tr>
+              <th scope="col">{{ __('Date') }}</th>
+              <th scope="col">{{ __('Human or Targeted') }}</th>
+              <th scope="col">{{ __('Bots') }}</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($honeypot['counts'] as $count)
+            <tr>
+              <th scope="row">{{ \Illuminate\Support\Str::after($count['date'], '-') }}</th>
+              <td style="--size: calc({{ $count['human_or_targeted'] }} / {{ $honeypot['max'] }});">
+                <span class="data">{{ $count['human_or_targeted'] }}</span>
+                <span class="tooltip">{{ __('Human or Targeted') }}: {{ $count['human_or_targeted'] }}</span>
+              </td>
+              <td style="--size: calc({{ $count['not_human_or_targeted'] }} / {{ $honeypot['max'] }});">
+                <span class="data">{{ $count['not_human_or_targeted'] }}</span>
+                <span class="tooltip">{{ __('Bots') }}: {{ $count['not_human_or_targeted'] }}</span>
+              </td>
+            </tr>
+            @endforeach
+            </tbody>
+          </table>
+        </div>
+        @endif
+        @if(isset($most_recent_honeypot_events[$honeypot['name']]))
+        <div class="card-text mb-3">
+          <table class="table">
+            <thead>
+            <tr>
+              <th colspan="3">
+                {!! __('The&nbsp;<span style="color: var(--c-orange-light);">5</span>&nbsp;most recent attacks') !!}
+              </th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($most_recent_honeypot_events[$honeypot['name']]['events'] as $event)
+            <tr title="{{ $event['event_details'] }}">
+              <td style="color: var(--c-blue);">
+                @if($event['attacker_name'] !== '-')
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                     class="icon icon-tabler icons-tabler-outline icon-tabler-user">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                  <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"/>
+                  <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"/>
+                </svg>
+                @else
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                     class="icon icon-tabler icons-tabler-outline icon-tabler-robot">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                  <path d="M6 4m0 2a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v4a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2z"/>
+                  <path d="M12 2v2"/>
+                  <path d="M9 12v9"/>
+                  <path d="M15 12v9"/>
+                  <path d="M5 16l4 -2"/>
+                  <path d="M15 14l4 2"/>
+                  <path d="M9 18h6"/>
+                  <path d="M10 8v.01"/>
+                  <path d="M14 8v.01"/>
+                </svg>
+                @endif
+              </td>
+              <td>
+                {{ $event['timestamp'] }}
+              </td>
+              <td>
+                {{ $event['event_type'] }}
+              </td>
+            </tr>
+            @endforeach
+            </tbody>
+          </table>
+        </div>
+        @endif
+      </div>
+    </div>
+  </div>
+  @endforeach
 </div>
 @endif
-<!-- HONEYPOTS -->
-<div class="row pt-3">
-  <div class="col-4">
-
-  </div>
-  <div class="col-4">
-
-  </div>
-  <div class="col-4">
-
-  </div>
-</div>
+<!-- HONEYPOTS : END -->
 @endsection
