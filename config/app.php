6726d92efd998c37ccd4541558dcbf461e5f508c
Patrick Brisacier <pbrisacier@mncc.fr>
2025-06-16 17:28:35 +0200

    Activate HealthServiceProvider

--- config/app.php
+++ config/app.php
@@ -198,6 +198,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\HealthServiceProvider::class,
         App\Providers\Filament\AdminPanelProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\FolioServiceProvider::class,
