fd4af009a2395b8f29e0a5e064e605246d87616c
csavelief <csavelief@mncc.fr>
2025-06-26 12:44:41 +0200

    TPE-PME 4/n

--- resources/themes/cywise/pages/tpe-pme/index.blade.php
+++ resources/themes/cywise/pages/tpe-pme/index.blade.php
@@ -229,5 +229,109 @@ name('tpe-pme');
       </div>
     </div>
     <!-- SOLUTION : END -->
+    <!-- FEATURES : BEGIN-->
+    <div class="bg-white py-24 sm:py-32">
+      <div class="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
+        <h2 class="text-base/7 font-semibold text-indigo-600">
+          {{ __('TPE_PME_FEATURE_SECTION') }}
+        </h2>
+        <p class="mt-2 max-w-lg text-pretty text-4xl font-semibold tracking-tight text-gray-950 sm:text-5xl">
+          {{ __('TPE_PME_FEATURE_TITLE') }}
+        </p>
+        <div class="mt-10 grid grid-cols-1 gap-4 sm:mt-16 lg:grid-cols-6 lg:grid-rows-2">
+          <div class="relative lg:col-span-3">
+            <div class="absolute inset-0 rounded-lg bg-white max-lg:rounded-t-[2rem] lg:rounded-tl-[2rem]"></div>
+            <div class="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)] lg:rounded-tl-[calc(2rem+1px)]">
+              <img class="h-80 object-cover object-left" src="https://tailwindcss.com/plus-assets/img/component-images/bento-01-performance.png" alt="" />
+              <div class="p-10 pt-4">
+                <h3 class="text-sm/4 font-semibold text-indigo-600">
+                  {{ __('TPE_PME_FEATURE_SECTION_1') }}
+                </h3>
+                <p class="mt-2 text-lg font-medium tracking-tight text-gray-950">
+                  {{ __('TPE_PME_FEATURE_TITLE_1') }}
+                </p>
+                <p class="mt-2 max-w-lg text-sm/6 text-gray-600">
+                  {{ __('TPE_PME_FEATURE_TEXT_1') }}
+                </p>
+              </div>
+            </div>
+            <div class="pointer-events-none absolute inset-0 rounded-lg shadow outline outline-black/5 max-lg:rounded-t-[2rem] lg:rounded-tl-[2rem]"></div>
+          </div>
+          <div class="relative lg:col-span-3">
+            <div class="absolute inset-0 rounded-lg bg-white lg:rounded-tr-[2rem]"></div>
+            <div class="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-tr-[calc(2rem+1px)]">
+              <img class="h-80 object-cover object-left lg:object-right" src="https://tailwindcss.com/plus-assets/img/component-images/bento-01-releases.png" alt="" />
+              <div class="p-10 pt-4">
+                <h3 class="text-sm/4 font-semibold text-indigo-600">
+                  {{ __('TPE_PME_FEATURE_SECTION_2') }}
+                </h3>
+                <p class="mt-2 text-lg font-medium tracking-tight text-gray-950">
+                  {{ __('TPE_PME_FEATURE_TITLE_2') }}
+                </p>
+                <p class="mt-2 max-w-lg text-sm/6 text-gray-600">
+                  {{ __('TPE_PME_FEATURE_TEXT_2') }}
+                </p>
+              </div>
+            </div>
+            <div class="pointer-events-none absolute inset-0 rounded-lg shadow outline outline-black/5 lg:rounded-tr-[2rem]"></div>
+          </div>
+          <div class="relative lg:col-span-2">
+            <div class="absolute inset-0 rounded-lg bg-white lg:rounded-bl-[2rem]"></div>
+            <div class="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-bl-[calc(2rem+1px)]">
+              <img class="h-80 object-cover object-left" src="https://tailwindcss.com/plus-assets/img/component-images/bento-01-speed.png" alt="" />
+              <div class="p-10 pt-4">
+                <h3 class="text-sm/4 font-semibold text-indigo-600">
+                  {{ __('TPE_PME_FEATURE_SECTION_3') }}
+                </h3>
+                <p class="mt-2 text-lg font-medium tracking-tight text-gray-950">
+                  {{ __('TPE_PME_FEATURE_TITLE_3') }}
+                </p>
+                <p class="mt-2 max-w-lg text-sm/6 text-gray-600">
+                  {{ __('TPE_PME_FEATURE_TEXT_3') }}
+                </p>
+              </div>
+            </div>
+            <div class="pointer-events-none absolute inset-0 rounded-lg shadow outline outline-black/5 lg:rounded-bl-[2rem]"></div>
+          </div>
+          <div class="relative lg:col-span-2">
+            <div class="absolute inset-0 rounded-lg bg-white"></div>
+            <div class="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)]">
+              <img class="h-80 object-cover" src="https://tailwindcss.com/plus-assets/img/component-images/bento-01-integrations.png" alt="" />
+              <div class="p-10 pt-4">
+                <h3 class="text-sm/4 font-semibold text-indigo-600">
+                  {{ __('TPE_PME_FEATURE_SECTION_4') }}
+                </h3>
+                <p class="mt-2 text-lg font-medium tracking-tight text-gray-950">
+                  {{ __('TPE_PME_FEATURE_TITLE_4') }}
+                </p>
+                <p class="mt-2 max-w-lg text-sm/6 text-gray-600">
+                  {{ __('TPE_PME_FEATURE_TEXT_4') }}
+                </p>
+              </div>
+            </div>
+            <div class="pointer-events-none absolute inset-0 rounded-lg shadow outline outline-black/5"></div>
+          </div>
+          <div class="relative lg:col-span-2">
+            <div class="absolute inset-0 rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-br-[2rem]"></div>
+            <div class="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-br-[calc(2rem+1px)]">
+              <img class="h-80 object-cover" src="https://tailwindcss.com/plus-assets/img/component-images/bento-01-network.png" alt="" />
+              <div class="p-10 pt-4">
+                <h3 class="text-sm/4 font-semibold text-indigo-600">
+                  {{ __('TPE_PME_FEATURE_SECTION_5') }}
+                </h3>
+                <p class="mt-2 text-lg font-medium tracking-tight text-gray-950">
+                  {{ __('TPE_PME_FEATURE_TITLE_5') }}
+                </p>
+                <p class="mt-2 max-w-lg text-sm/6 text-gray-600">
+                  {{ __('TPE_PME_FEATURE_TEXT_5') }}
+                </p>
+              </div>
+            </div>
+            <div class="pointer-events-none absolute inset-0 rounded-lg shadow outline outline-black/5 max-lg:rounded-b-[2rem] lg:rounded-br-[2rem]"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- FEATURES : END -->
   </x-container>
 </x-layouts.marketing>
