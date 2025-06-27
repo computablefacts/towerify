0782cfbfab91a03b432e665be07f7119541d3e43
csavelief <csavelief@mncc.fr>
2025-06-27 15:11:58 +0200

    TPE-PME 6/n

--- resources/themes/cywise/pages/tpe-pme/index.blade.php
+++ resources/themes/cywise/pages/tpe-pme/index.blade.php
@@ -34,7 +34,7 @@ name('tpe-pme');
           </p>
           <div class="mt-10 flex items-center gap-x-6">
             <a href="{{ route('tools.cybercheck.init') }}" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-              {{ __('TPE_PME_CTA') }}
+              {{ __('TPE_PME_CTA_1') }}
             </a>
             <a href="{{ route('register') }}" class="text-sm/6 font-semibold text-gray-900">
               {{ __('TPE_PME_REGISTER') }} <span aria-hidden="true">→</span>
@@ -381,5 +381,22 @@ name('tpe-pme');
       </div>
     </section>
     <!-- TESTIMONIALS : END -->
+    <!-- PRICING : BEGIN -->
+    <x-marketing.sections.pricing />
+    <!-- PRICING : END -->
+    <!-- CTA : BEGIN -->
+    <div class="bg-white">
+      <div class="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:justify-between lg:px-8">
+        <h2 class="max-w-2xl text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+          {!! __('TPE_PME_CTA_2') !!}
+        </h2>
+        <div class="mt-10 flex items-center gap-x-6 lg:mt-0 lg:shrink-0">
+          <a href="{{ route('tools.cybercheck.init') }}" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            {{ __('TPE_PME_CTA_1') }}
+          </a>
+        </div>
+      </div>
+    </div>
+    <!-- CTA : END -->
   </x-container>
 </x-layouts.marketing>
