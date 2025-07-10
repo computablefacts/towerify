680ccc53149c224dfbfd3a9651abbd9cc2b1b5df
csavelief <csavelief@mncc.fr>
2025-07-10 16:43:29 +0200

    Increase Scout's chunk size

--- config/scout.php
+++ config/scout.php
@@ -71,8 +71,8 @@ return [
     */
 
     'chunk' => [
-        'searchable' => 500,
-        'unsearchable' => 500,
+        'searchable' => 10_000,
+        'unsearchable' => 10_000,
     ],
 
     /*
